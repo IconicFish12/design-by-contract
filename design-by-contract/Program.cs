7fbb751a9bcf6bbbd2fea1c0f75ade2ec0180d3d
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-03-23 19:50:48 +0700

    Add project files.

---
+++ design-by-contract/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ design-by-contract/SayaTubeVideo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace design_by_contract
+{
+    class SayaTubeVideo
+    {
+        private int id, playcount;
+        private string title;
+
+        Random rnd = new Random();
+
+        public SayaTubeVideo(string title)
+        {
+            this.id = rnd.Next(1, 100);
+            this.title = title;
+            this.playcount = 0;
+        }
+
+        
+    }
+}
