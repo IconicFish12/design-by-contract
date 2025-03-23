59820b4467714e76110c3f05718eb56bf88d0e58
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-03-23 20:42:51 +0700

    mengimplementasi Design By Contract

--- design-by-contract/Program.cs
+++ design-by-contract/Program.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using design_by_contract;
+using System;
 using System.Runtime.CompilerServices;
 
 class Program
 {
     public static void Main(string[] args)
     {
-        
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial C#");
+
+        for (int i = 0; i < 100; i++)
+        {
+            video.increasePlayCount(1_000_000);
+        }
+
+        video.printVideoDetails();
     }
 }
--- design-by-contract/SayaTubeVideo.cs
+++ design-by-contract/SayaTubeVideo.cs
@@ -1,33 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace design_by_contract
 {
     class SayaTubeVideo
     {
-        private int id, playcount = 0;
+        private int id, playcount;
         private string title;
 
         Random rnd = new Random();
 
         public SayaTubeVideo(string title)
         {
+            Debug.Assert(!string.IsNullOrEmpty(title), "Judul tidak boleh null atau kosong.");
+            Debug.Assert(title.Length <= 100, "Judul tidak boleh lebih dari 100 karakter.");
+
             this.id = rnd.Next(1, 100);
             this.title = title;
+            this.playcount = 0;
         }
 
         public void increasePlayCount(int count)
         {
-            
+            Debug.Assert(count <= 10_000_000, "Input penambahan play count melebihi batas 10.000.000 per panggilan.");
+
+            try
+            {
+                checked
+                {
+                    playcount += count;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
 
         public void printVideoDetails()
         {
-
+            Console.WriteLine($"Video dengan Judul {title}, pada id {id}, dengan playcount {playcount}");
         }
     }
 }
